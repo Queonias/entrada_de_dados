e080f6348b271dba65019fbcfe81e9708b4f9ebf
queonias <queones@gmail.com>
2023-11-09 21:43:43 -0300

    simples pedometer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,135 @@
-import 'package:entrada_dados/campo_texto.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
 
 void main() {
-  runApp(const MaterialApp(
-    home: CampoTexto(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  // Definir o método initPlatformState
+  void initPlatformState() async {
+    // Colocar o código que você quer executar aqui
+    // Por exemplo, verificar e solicitar a permissão de reconhecimento de atividade, e iniciar o stream do pedometer se a permissão for concedida
+    if (await Permission.activityRecognition.request().isGranted) {
+      // Iniciar o stream do pedometer
+      _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+      _pedestrianStatusStream
+          .listen(onPedestrianStatusChanged)
+          .onError(onPedestrianStatusError);
+
+      _stepCountStream = Pedometer.stepCountStream;
+      _stepCountStream.listen(onStepCount).onError(onStepCountError);
+    } else {
+      // Mostrar uma mensagem de erro ou solicitar a permissão novamente
+      setState(() {
+        _status = 'Permissão negada';
+        _steps = '0';
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Chamar o método initPlatformState no método initState
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pedometer Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Steps Taken',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                _steps,
+                style: const TextStyle(fontSize: 60),
+              ),
+              const Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              const Text(
+                'Pedestrian Status',
+                style: TextStyle(fontSize: 30),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? const TextStyle(fontSize: 30)
+                      : const TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
