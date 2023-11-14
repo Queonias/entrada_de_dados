66c8489a8f65cf4b8399c1e557c16386bcc90fe6
queonias <queones@gmail.com>
2023-11-14 13:48:39 -0300

    done

--- lib/api.dart
+++ lib/api.dart
@@ -4,26 +4,9 @@ import 'package:entrada_dados/model/video.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-const ID_CANAL = "UC28hwHqqtAOY6xF_cf1s-ww";
-const URL_BASE = "https://www.googleapis.com/youtube/v3/";
 String KEY_API = dotenv.get('CHAVE_TOUTUBE_API');
 
 class Api {
-  final httpsUri = Uri(
-    scheme: 'https',
-    host: 'www.googleapis.com',
-    path: 'youtube/v3/search',
-    fragment: 'numbers',
-    queryParameters: {
-      'part': 'snippet',
-      'type': 'video',
-      'maxResults': '20',
-      'order': 'date',
-      'key': KEY_API,
-      'q': 'date'
-    },
-  );
-
   Future<List<Video>> pesquisa(String pesquisa) async {
     final httpsUri = Uri(
       scheme: 'https',
