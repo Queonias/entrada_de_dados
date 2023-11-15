ff57f4a5a6be6229ff44ec22afd11f9f61b900e3
queonias <queones@gmail.com>
2023-11-15 19:11:05 -0300

    112. #App8 Lista de tarefas - Salvando dados utilizando arquivo

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,9 +13,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final List _listaTarefas = ["Ir ao mercado", "Estudar", "Tomar banho"];
+  final List _listaTarefas = [];
+
+  _salvarArquivo() async {
+    final diretorio = await getApplicationCacheDirectory();
+    var arquivo = File("${diretorio.path}/dados.json");
+
+    Map<String, dynamic> tarefa = {};
+    tarefa["titulo"] = "Ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+
   @override
   Widget build(BuildContext context) {
+    _salvarArquivo();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Lista de tarefas"),
@@ -53,7 +73,7 @@ class _HomeState extends State<Home> {
               itemCount: _listaTarefas.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(_listaTarefas[index]),
+                  title: Text(_listaTarefas[index]["titulo"].toString()),
                 );
               },
             ),
