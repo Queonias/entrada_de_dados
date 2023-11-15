1e4fdf3de3b6e12fc7494deb236f2b446323df72
queonias <queones@gmail.com>
2023-11-15 17:19:58 -0300

    111. #App8 Lista de tarefas - Criando interface

--- lib/home.dart
+++ lib/home.dart
@@ -8,11 +8,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List _listaTarefas = ["Ir ao mercado", "Estudar", "Tomar banho"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text("Lista de tarefas"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text("Adcionar tarefa"),
+                content: TextField(
+                  decoration:
+                      const InputDecoration(labelText: "Digite sua tarefa"),
+                  onChanged: (value) {},
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Salvar")),
+                  TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("Cancelar"))
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaTarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_listaTarefas[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
