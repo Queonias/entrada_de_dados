87d94f23d24d8e79fc525cccecf6a509c3414695
queonias <queones@gmail.com>
2023-11-15 14:54:19 -0300

    #App8 Lista de tarefas - Floating Action button

--- lib/home.dart
+++ lib/home.dart
@@ -14,6 +14,30 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text("Home"),
       ),
+      body: const Text("Conteúdo"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+        elevation: 6,
+        icon: const Icon(Icons.add_shopping_cart), // extended
+        label: const Text("Adcionar"), onPressed: () {}, //extended
+        // shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(20)), //extended
+        // mini: true, // centerDocked
+        // child: const Icon(Icons.add), //centerDocked
+        // onPressed: () {
+        //   print("Resultado: Botão pressionado"); //centerDocked
+        // },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        // shape: const CircularNotchedRectangle(),
+        child: Row(
+          children: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
+          ],
+        ),
+      ),
     );
   }
 }
