58cd012679c760f1bee770c7865aa4a1a292906c
queonias <queones@gmail.com>
2023-11-16 14:14:11 -0300

    119. Configurando banco de dados

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +10,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  _recuperaBancoDados() async {
+    final caminhoBancoDados = await getDatabasesPath();
+    final localBancoDados = join(caminhoBancoDados, "banco.db");
+
+    var retorno = await openDatabase(
+      localBancoDados,
+      version: 1,
+      onCreate: (db, version) {
+        String sql =
+            "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
+
+        db.execute(sql);
+      },
+    );
+
+    print("aberto ${retorno.isOpen.toString()}");
+  }
+
   @override
   Widget build(BuildContext context) {
+    _recuperaBancoDados();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
