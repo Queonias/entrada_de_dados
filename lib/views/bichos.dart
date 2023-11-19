5d77416d962fce04164902d0b14dcc801c033d40
queonias <queones@gmail.com>
2023-11-19 20:10:58 -0300

    138. #App10 Aprenda Inglês - Criando interface com GridView & MediaQuery

--- lib/views/bichos.dart
+++ lib/views/bichos.dart
@@ -10,8 +10,41 @@ class Bichos extends StatefulWidget {
 class _BichosState extends State<Bichos> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Bixos"),
+    // double largura = MediaQuery.of(context).size.width;
+    // double altura = MediaQuery.of(context).size.height;
+
+    return Center(
+      child: GridView.count(
+        crossAxisCount: 2,
+        scrollDirection: Axis.vertical,
+        childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset("assets/imagens/cao.png"),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset("assets/imagens/gato.png"),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset("assets/imagens/leao.png"),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset("assets/imagens/macaco.png"),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset("assets/imagens/ovelha.png"),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset("assets/imagens/vaca.png"),
+          ),
+        ],
+      ),
     );
   }
 }
