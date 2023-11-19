cd273928c3009977550804d8bf07b6c7032a9f7e
queonias <queones@gmail.com>
2023-11-19 16:55:57 -0300

    135. Controlando volume

--- lib/home.dart
+++ lib/home.dart
@@ -10,24 +10,79 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   AudioPlayer audioPlayer = AudioPlayer();
+  double volume = 0.5;
 
   _executar() async {
     String url = "audios/musica.mp3";
     await audioPlayer.play(AssetSource(url));
   }
 
+  _pausar() async {
+    await audioPlayer.pause();
+  }
+
+  _parar() async {
+    await audioPlayer.stop();
+  }
+
   @override
   void initState() {
     super.initState();
-    _executar();
   }
 
   @override
   Widget build(BuildContext context) {
-    // _executar();
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text("Executando sons"),
+      ),
+      body: Column(
+        children: [
+          Slider(
+            value: volume,
+            min: 0,
+            max: 1,
+            divisions: 10,
+            onChanged: (value) {
+              setState(() {
+                volume = value;
+              });
+              audioPlayer.setVolume(volume);
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: GestureDetector(
+                  onTap: () {
+                    _executar();
+                  },
+                  child: Image.asset("assets/imagens/executar.png"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: GestureDetector(
+                  onTap: () {
+                    _pausar();
+                  },
+                  child: Image.asset("assets/imagens/pausar.png"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: GestureDetector(
+                  onTap: () {
+                    _parar();
+                  },
+                  child: Image.asset("assets/imagens/parar.png"),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
