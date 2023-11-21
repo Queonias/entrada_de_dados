1979c7aec7aca64b777bd1a87d3a1c375f99a2c6
queonias <queones@gmail.com>
2023-11-21 08:41:44 -0300

    149. Salvando & atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:entrada_dados/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_options.dart'; // Verifique se este arquivo contém as opções corretas do Firebase
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
@@ -10,12 +10,55 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseFirestore.instance
-      .collection("collectionPath")
-      .doc("pontuacao")
-      .set({"Queonias": "250", "Ana": "560"});
+  runApp(const MyApp());
+}
 
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Move a operação de escrita para um local apropriado no seu app, não na função main
+    // _adicionarUsuario();
+
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+
+  Future<void> _adicionarUsuario() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+
+    //   try {
+    //     await db
+    //         .collection("usuarios")
+    //         .doc("001")
+    //         .set({"nome": "Queonias", "idade": "31"});
+    //     print("Documento adicionado com sucesso!");
+    //   } catch (e) {
+    //     print("Erro ao adicionar documento: $e");
+    //   }
+    // }
+
+    //   try {
+    //     DocumentReference ref = await db.collection("noticias").add({
+    //       "titilo": "criada nova moeda virtual",
+    //       "descricao": "texto exemplo"
+    //     });
+    //     print("item salvo ${ref.id} ");
+    //   } catch (e) {
+    //     print("Erro ao adicionar documento: $e");
+    //   }
+    // }
+
+  //   try {
+  //     await db.collection("noticias").doc("FFWw3c69ERlN2FiOAMdf").set({
+  //       "descricao": "Ondas de calor",
+  //       "titilo": "Tá muito quente aqui eu tô morrendo de calor, c",
+  //     });
+  //     print("item salvo");
+  //   } catch (e) {
+  //     print("Erro ao adicionar documento: $e");
+  //   }
+  // }
 }
