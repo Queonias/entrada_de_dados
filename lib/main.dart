efc7a0c175d65444b6cd544418280ceca53f0add
queonias <queones@gmail.com>
2023-11-22 13:33:50 -0300

    150. Removendo & recuperando

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Move a operação de escrita para um local apropriado no seu app, não na função main
-    // _adicionarUsuario();
+    _adicionarUsuario();
 
     return const MaterialApp(
       home: Home(),
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
   Future<void> _adicionarUsuario() async {
     FirebaseFirestore db = FirebaseFirestore.instance;
 
+    // Adicionando Usuário
     //   try {
     //     await db
     //         .collection("usuarios")
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
     //   }
     // }
 
+    // Adicionando usuário com nº do documento variável
     //   try {
     //     DocumentReference ref = await db.collection("noticias").add({
     //       "titilo": "criada nova moeda virtual",
@@ -51,6 +53,7 @@ class MyApp extends StatelessWidget {
     //   }
     // }
 
+    //  Auterando documento
     //   try {
     //     await db.collection("noticias").doc("FFWw3c69ERlN2FiOAMdf").set({
     //       "descricao": "Ondas de calor",
@@ -61,5 +64,64 @@ class MyApp extends StatelessWidget {
     //     print("Erro ao adicionar documento: $e");
     //   }
     // }
+
+    //  Auterando documento
+    // try {
+    //   await db.collection("usuarios").doc("001").delete();
+    //   print("item deletado");
+    // } catch (e) {
+    //   print("Erro ao adicionar documento: $e");
+    // }
+
+    // Recuperando usuário
+    // DocumentSnapshot snapshot =
+    //     await db.collection("usuarios").doc("002").get();
+
+    // if (snapshot.exists) {
+    //   Map<String, dynamic>? dados = snapshot.data() as Map<String, dynamic>?;
+    //   if (dados != null) {
+    //     print("Dados: ${dados['nome']}");
+    //   } else {
+    //     print('Dados é nulo ou não é um Map<String, dynamic>');
+    //   }
+    // } else {
+    //   print('Documento não encontrado');
+    // }
+
+    // Recuperando Lista completa
+    //   try {
+    //     QuerySnapshot querySnapshot = await db.collection('usuarios').get();
+
+    //     if (querySnapshot.docs.isNotEmpty) {
+    //       // Iterando por todos os documentos da coleção
+    //       querySnapshot.docs.forEach((document) {
+    //         Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+    //         print('ID do documento: ${document.id}');
+    //         print('Dados: $data');
+    //         // Aqui você pode fazer o que quiser com os dados de cada documento
+    //       });
+    //     } else {
+    //       print('Nenhum documento encontrado na coleção "usuarios".');
+    //     }
+    //   } catch (e) {
+    //     print('Erro ao buscar documentos: $e');
+    //   }
+
+    // escutando o db para avisar ao atualizar o banco
+
+    try {
+      var collection = db.collection('usuarios');
+      var subscription =
+          collection.snapshots().listen((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((document) {
+          print("Dados: ${document.data()}");
+        });
+      });
+
+      // Para cancelar a assinatura do stream quando não precisar mais
+      // subscription.cancel();
+    } catch (e) {
+      print('Erro ao buscar documentos: $e');
+    }
   }
 }
